f1916bed2cb725e9c1e9d85291061b5c85fef030
JRShreevalli <70849567+JRShreevalli@users.noreply.github.com>
2022-01-08 11:46:35 +0530

    Create First_And_Last_Digit.cpp

---
+++ Beginner/First_And_Last_Digit.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std ;
+
+int Pro(int val)
+{
+    int last = val % 10 ;
+    int first ; 
+    while(val > 9)
+    {
+        val /= 10 ; 
+    }
+    return last + val % 10 ;
+}
+int main()
+{
+    int t , i , n ;
+    cin >> t ;
+    for(i=0 ; i<t ; i += 1)
+    {
+        cin >> n ;
+        cout << Pro(n) << "\n" ;
+    }
+    return 0 ;
+}
