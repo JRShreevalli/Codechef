5683cef2f6215b2e4a526f65264bad42fab9b70b
JRShreevalli <70849567+JRShreevalli@users.noreply.github.com>
2022-01-08 11:15:16 +0530

    Create Add_two_numbers.c

---
+++ Beginner/Add_two_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+
+int main() {
+	int T;
+	scanf("%d",&T);
+	while (T--) {
+	
+		int a, b, ans;
+		scanf("%d %d", &a, &b);
+
+	
+		ans = a + b;
+		printf("%d\n", ans);
+	}
+
+
+}
