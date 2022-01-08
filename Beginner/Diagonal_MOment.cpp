ed3c399f2e225e6db7e8213ffc650472dc140aef
JRShreevalli <70849567+JRShreevalli@users.noreply.github.com>
2022-01-08 15:08:03 +0530

    Create Diagonal_MOment.cpp

---
+++ Beginner/Diagonal_MOment.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x,y;
+	    cin>>x>>y;
+	    
+	    if((abs(x + y))%2 == 0)
+	        cout<<"YES";
+	    else
+	        cout<<"NO";
+	    
+	    cout<<endl;
+	}
+	return 0;
+}
