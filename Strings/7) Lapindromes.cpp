64a49eb6d5f619749b73d7882e6b22e54a2b1473
J Shreevalli <70849567+JRShreevalli@users.noreply.github.com>
2022-05-10 16:14:54 +0530

    Create 7) Lapindromes.cpp

---
+++ Strings/7) Lapindromes.cpp
@@ -0,0 +1,45 @@
+Problem Statement :
+                    Lapindrome is defined as a string which when split in the middle, gives two halves having 
+                    the same characters and same frequency of each character. If there are odd number of characters in the string,
+                    we ignore the middle character and check for lapindrome. For example gaga is a lapindrome, 
+                    since the two halves ga and ga have the same characters with same frequency. 
+                    Also, abccab, rotor and xyzxy are a few examples of lapindromes. 
+                    Note that abbaab is NOT a lapindrome. The two halves contain the same characters but their 
+                    frequencies do not match.
+                    Your task is simple. Given a string, you need to tell if it is a lapindrome.
+                      
+Problem Code : LAPIN 
+
+// SOLUTION //
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        string first, second;
+        int n = s.length();
+        for(int i=0;i<n/2;i++)
+        {
+            first = first + s[i];
+            second = second + s[n-i-1];
+        }
+        sort(first.begin(), first.end());
+        sort(second.begin(), second.end());
+        
+        if(first == second)
+        {
+            cout<<"YES\n";
+        }
+        else
+        {
+            cout<<"NO\n";
+        }
+    }
+}
