ef25f856b1757c3f0775ea442e519a247cac08bd
J Shreevalli <70849567+JRShreevalli@users.noreply.github.com>
2022-05-10 16:01:34 +0530

    Create 1) Pawri Meme.cpp

---
+++ Strings/1) Pawri Meme.cpp
@@ -0,0 +1,47 @@
+Problem Question :
+                  Lately, Chef has been inspired by the "pawri" meme. Therefore, he decided to take a string S 
+                  and change each of its substrings that spells "party" to "pawri". Find the resulting string.
+                    
+Problem Code: PAWRI
+
+// SOLUTION // 
+
+#include <iostream>
+#include <bits/stdc++.h>
+#include <string.h>
+using namespace std;
+
+int main() 
+{
+	int t ;
+	string str ;
+	cin >> t ;
+	while ( t-- )
+	{
+		cin >> str ;
+		int len = str.length() ;
+		int i ;
+
+		for( i = 0 ; i < len ; i++ )
+		{
+			if( str[i] == 'p' && str[i+1] == 'a' && str[i+2] == 'r' && str[i+3] == 't' && str[i+4] == 'y' )
+			{
+				str[i] = 'p' ;
+				str[i+1] = 'a' ;
+				str[i+2] = 'w' ;
+				str[i+3] = 'r' ;
+				str[i+4] = 'i' ;
+			}
+			else
+			{
+				str[i] = str[i] ;
+			}
+		}
+		for( i = 0 ; i < len ; i++ )
+		{
+				cout << str[i] ;
+		}
+		cout << endl ;
+	}
+	return 0 ;
+}
